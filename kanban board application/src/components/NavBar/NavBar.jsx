239101f763ac3f81a398e1de3815f8ea8bd27d80
Gaurav <83856198+GauravSamanta@users.noreply.github.com>
2023-08-14 19:26:15 +0530

    final

---
+++ kanban board application/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./NavBar.css"
+
+function Navbar({ groupBy, sortOption, onGroupByChange, onSortOptionChange }) {
+  const [showDisplayBox, setShowDisplayBox] = useState(false); // Initialize with false
+  
+  const toggleDisplayBox = () => {
+    setShowDisplayBox(!showDisplayBox); // Toggle the value
+  };
+
+  return (
+    <nav className="navbar ">
+      <div className="navbar-inner">
+        <div
+          className={`display-button-container ${showDisplayBox ? "show" : ""}`}
+        >
+          <button className="display-button" onClick={toggleDisplayBox}>
+            Display
+          </button>
+
+         
+          <div className={`button-box ${showDisplayBox ? "show" : ""}`}>
+              <div className="grouping-options">
+                <div className="select-container">
+                  <label htmlFor="groupSelect">Group by:</label>
+                  <select
+                    id="groupSelect"
+                    value={groupBy}
+                    onChange={onGroupByChange}
+                  >
+                    <option value="status">By Status</option>
+                    <option value="userId">By User</option>
+                    <option value="priority">By Priority</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="sorting-options">
+                <div className="select-container">
+                  <label htmlFor="sortSelect">Sort by:</label>
+                  <select
+                    id="sortSelect"
+                    value={sortOption}
+                    onChange={onSortOptionChange}
+                  >
+                    <option value="priority">Priority</option>
+                    <option value="title">Title</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ kanban board application/src/components/TicketCard/TicketCard.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./TicketCard.css"
+function TicketCard({ task, users }) {
+  const [isCompleted, setIsCompleted] = useState(task.completed || false);
+
+  useEffect(() => {
+    const savedCompletionState = localStorage.getItem(`completed_${task.id}`);
+
+    if (savedCompletionState) {
+      setIsCompleted(JSON.parse(savedCompletionState));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(`completed_${task.id}`, JSON.stringify(isCompleted));
+    console.log("isCompleted", isCompleted);
+    console.log("task.id", task.id);
+  }, [isCompleted]);
+
+  const handleToggleComplete = () => {
+    setIsCompleted(!isCompleted);
+  };
+
+  return (
+    <div className={`ticket-card ${isCompleted ? "completed" : ""}`}>
+      <div className="ticket-details">
+        <div className="card-body">
+          {task.id}
+          <h3 className="title"> {task.title}</h3> {/* Display id as title */}
+          <p className="tag">
+            {" "}
+            <span className="tag-text">{task.tag}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TicketCard;
